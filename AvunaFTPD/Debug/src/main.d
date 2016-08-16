200f36be5f5e0cbf8a582809b9ac9cd516dafb3c
JavaProphet <max.bruce12@gmail.com>
2016-08-15 23:56:14 -0700

    Fix building

---
+++ AvunaFTPD/Debug/src/main.d
@@ -0,0 +1,26 @@
+src/main.d: ../src/main.c ../src/config.h ../src/xstring.h \
+ ../src/version.h ../src/util.h ../src/streams.h ../src/accept.h \
+ ../src/collection.h ../src/work.h ../src/log.h ../src/globals.h \
+ ../src/tls.h
+
+../src/config.h:
+
+../src/xstring.h:
+
+../src/version.h:
+
+../src/util.h:
+
+../src/streams.h:
+
+../src/accept.h:
+
+../src/collection.h:
+
+../src/work.h:
+
+../src/log.h:
+
+../src/globals.h:
+
+../src/tls.h:
